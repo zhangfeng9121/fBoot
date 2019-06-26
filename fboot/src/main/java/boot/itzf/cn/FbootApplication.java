22e53d94db4efa0471e16c67549da6e97b56f7b1
zhangfeng <zhangfeng9121@163.com>
2019-06-26 22:56:59 +0800

    f-xin

---
+++ fboot/src/main/java/boot/itzf/cn/FbootApplication.java
@@ -0,0 +1,13 @@
+package boot.itzf.cn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FbootApplication.class, args);
+    }
+
+}
