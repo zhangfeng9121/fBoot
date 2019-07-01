494d38a0879a3a51d391f8c8c4de034d8fdc3f8c
zhangfeng <1056859084@qq.com>
2019-07-01 17:10:35 +0800

    add employ

--- fboot/src/main/java/boot/itzf/cn/control/EmployeeControl.java
+++ fboot/src/main/java/boot/itzf/cn/control/EmployeeControl.java
@@ -1,6 +1,8 @@
 package boot.itzf.cn.control;
 
+import boot.itzf.cn.dao.DepartmentDao;
 import boot.itzf.cn.dao.EmployeeDao;
+import boot.itzf.cn.entities.Department;
 import boot.itzf.cn.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -18,10 +20,31 @@ public class EmployeeControl {
     @Autowired
     private EmployeeDao employeeDao;
 
+    @Autowired
+    private DepartmentDao departmentDao;
+
+    /**
+     *  获取员工列表
+     * @param model
+     * @return
+     */
     @GetMapping("/emps")
     public String getList(Model model) {
         Collection<Employee> allList = employeeDao.getAll();
         model.addAttribute("emps", allList);
         return "emp/list";
+    }
+
+    /**
+     * 获取添加页面
+     * @param model
+     * @return
+     */
+    @GetMapping("/emp")
+    public String getaddPage(Model model) {
+        Collection<Department> departments = departmentDao.getDepartments();
+        System.out.println("=================" + departments.toString());
+        model.addAttribute("depts", departments);
+        return "emp/add";
     }
 }
